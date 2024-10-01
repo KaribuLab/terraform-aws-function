37c5a94578d4b81e40aab82fe4337ae26d49d264
Patricio Ascencio <patricio.ascencio@karibu.cl>
2024-10-01 03:31:14 -0300

    feat: VPC Config

--- input.tf
+++ input.tf
@@ -1,86 +1,96 @@
-variable function_name {
-  type = string
+variable "function_name" {
+  type        = string
   description = "Name of lambda function"
 }
 
-variable iam_policy {
-    type = string
-    description = "IAM policy"
+variable "iam_policy" {
+  type        = string
+  description = "IAM policy"
 }
 
-variable runtime {
-    type = string
-    description = "Runtime"
+variable "runtime" {
+  type        = string
+  description = "Runtime"
 }
 
-variable handler {
-    type = string
-    description = "Handler"
+variable "handler" {
+  type        = string
+  description = "Handler"
 
 }
 
-variable memory_size {
-    type = number
-    default = 128
-    description = "Memory size"
+variable "memory_size" {
+  type        = number
+  default     = 128
+  description = "Memory size"
 }
 
-variable timeout {
-    type = number
-    default = 30
-    description = "Timeout"
+variable "timeout" {
+  type        = number
+  default     = 30
+  description = "Timeout"
 }
 
-variable environment_variables {
-    type = map(string)
-    description = "Environment variables"
+variable "environment_variables" {
+  type        = map(string)
+  description = "Environment variables"
 }
 
-variable bucket {
-    type = string
-    description = "Bucket for lambda function"
+variable "bucket" {
+  type        = string
+  description = "Bucket for lambda function"
 }
 
-variable file_location {
-    type = string
-    description = "File location"
+variable "file_location" {
+  type        = string
+  description = "File location"
 }
 
-variable zip_location {
-    type = string
-    description = "Zip location"
+variable "zip_location" {
+  type        = string
+  description = "Zip location"
 }
 
-variable zip_name {
-    type = string
-    description = "Zip name"
+variable "zip_name" {
+  type        = string
+  description = "Zip name"
 }
 
-variable common_tags {
-    type = map(string)
-    description = "Common tags"
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags"
 }
 
-variable event_sources_arn {
-    type = list(string)
-    default = []
-    description = "Event sources arn"
+variable "event_sources_arn" {
+  type        = list(string)
+  default     = []
+  description = "Event sources arn"
 }
 
-variable batch_size {
-    type = number
-    default = 1
-    description = "Batch size"
+variable "batch_size" {
+  type        = number
+  default     = 1
+  description = "Batch size"
 }
 
-variable batch_window {
-    type = number
-    default = 0
-    description = "Batch window"
+variable "batch_window" {
+  type        = number
+  default     = 0
+  description = "Batch window"
 }
 
-variable max_concurrency {
-    type = number
-    default = -1
-    description = "Max concurrency"
+variable "max_concurrency" {
+  type        = number
+  default     = -1
+  description = "Max concurrency"
+}
+
+variable "vpc_config" {
+  type = object({
+    subnet_ids         = list(string)
+    security_group_ids = list(string)
+  })
+  description = "VPC configuration"
+  default     = null
+
 }
--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 locals {
-  iam_role_name = "${var.function_name}-execution-role"
-  iam_policy_name = "${var.function_name}-policy"
+  iam_role_name     = "${var.function_name}-execution-role"
+  iam_policy_name   = "${var.function_name}-policy"
   zip_name_location = "${var.zip_location}/${var.zip_name}"
+  iam_policy_map    = jsondecode(var.iam_policy)
 }
 
 resource "aws_iam_role" "function" {
@@ -22,14 +23,51 @@ resource "aws_iam_role" "function" {
   tags = var.common_tags
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 resource "aws_iam_role_policy" "function" {
   name = local.iam_policy_name
   role = aws_iam_role.function.id
-  policy = var.iam_policy
+  policy = var.vpc_config == null ? var.iam_policy : jsonencode({
+    Version = local.iam_policy_map.Version
+    Statement = concat(local.iam_policy_map.Statement, [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeSubnets",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+        ]
+        Resource = [
+          "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:subnet/*",
+          "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:vpc/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ]
+        Resource = "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"
+      }
+    ])
+  })
 }
 
 data "archive_file" "function" {
-  type = "zip"
+  type        = "zip"
   source_dir  = var.file_location
   output_path = local.zip_name_location
 }
@@ -42,20 +80,29 @@ resource "aws_s3_object" "function" {
   bucket = data.aws_s3_bucket.function.id
   key    = "${var.function_name}/${var.zip_name}"
   source = data.archive_file.function.output_path
-  etag = filemd5(data.archive_file.function.output_path)
+  etag   = filemd5(data.archive_file.function.output_path)
 }
 
 resource "aws_lambda_function" "function" {
-  function_name = "${var.function_name}"
-  role = aws_iam_role.function.arn
-  s3_bucket = data.aws_s3_bucket.function.id
-  s3_key    = aws_s3_object.function.key
-  source_code_hash = data.archive_file.function.output_base64sha256
-  runtime = var.runtime
-  handler = var.handler
-  memory_size = var.memory_size
+  function_name                  = var.function_name
+  role                           = aws_iam_role.function.arn
+  s3_bucket                      = data.aws_s3_bucket.function.id
+  s3_key                         = aws_s3_object.function.key
+  source_code_hash               = data.archive_file.function.output_base64sha256
+  runtime                        = var.runtime
+  handler                        = var.handler
+  memory_size                    = var.memory_size
   reserved_concurrent_executions = var.max_concurrency
-  timeout = var.timeout
+  timeout                        = var.timeout
+
+  dynamic "vpc_config" {
+    for_each = var.vpc_config != null ? [var.vpc_config] : []
+    content {
+      subnet_ids         = vpc_config.value.subnet_ids
+      security_group_ids = vpc_config.value.security_group_ids
+    }
+
+  }
   environment {
     variables = var.environment_variables
   }
@@ -63,10 +110,10 @@ resource "aws_lambda_function" "function" {
 }
 
 resource "aws_lambda_event_source_mapping" "function" {
-    count = length(var.event_sources_arn)
-    event_source_arn = var.event_sources_arn[count.index]
-    enabled = true
-    function_name = aws_lambda_function.function.arn
-    batch_size = var.batch_size
-    maximum_batching_window_in_seconds = var.batch_window
+  count                              = length(var.event_sources_arn)
+  event_source_arn                   = var.event_sources_arn[count.index]
+  enabled                            = true
+  function_name                      = aws_lambda_function.function.arn
+  batch_size                         = var.batch_size
+  maximum_batching_window_in_seconds = var.batch_window
 }
